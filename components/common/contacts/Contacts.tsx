ced4dc3ef8acdbe2bc2fd8ba41a21640ac216d9a
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-13 21:26:22 +0300

    feat: Implemented Contacts component for dynamic contact information display
    - Added the `Contacts` component to dynamically render contact links such as phone and email from `linkContacts.json`.
    - Utilized the `Icon` component from `@/components/ui/icon/Icon` to display relevant icons for phone and email, enhancing visual identification for users.
    - Styled the component using Tailwind CSS for responsive visibility, hiding it on smaller screens and displaying on medium screens and above.
    - Added interaction states (hover and focus) to improve accessibility and user experience, with responsive text sizes for different screen resolutions.
    - Contact links are designed to be accessible with appropriate aria-label attributes and interactive feedback on hover and focus, ensuring compliance with accessibility standards.
    
    This change introduces an intuitive and visually appealing way for users to access contact information, ensuring it adjusts seamlessly across different device sizes.

---
+++ components/common/contacts/Contacts.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Icon, {
+  commonIcons,
+  iconSizes,
+  iconTypes,
+} from '@/components/ui/icon/Icon';
+import data from '@/shared/data/common.json';
+import linkContacts from '@/shared/data/linkContacts.json';
+
+const Contacts = () => {
+  const { phone, mail } = data.icons;
+  return (
+    <ul className='sOnly:hidden items-center justify-between gap-4 md:flex md:gap-6 smOnly:hidden'>
+      {linkContacts.map(({ name, href, ariaLabel, label }) => (
+        <li
+          key={name}
+          className='text-slate-800 duration-250 cursor-pointer text-xs transition-all ease-in-out hover:text-blue-90 focus:text-accent dark:text-white hover:dark:text-blue-60 xl:text-base'
+        >
+          <a
+            href={href}
+            aria-label={ariaLabel}
+            className='flex items-center justify-start gap-2'
+          >
+            <Icon
+              type={iconTypes.COMMON}
+              name={name === 'phone' ? commonIcons.PHONE : commonIcons.MAIL}
+              size={name === 'phone' ? iconSizes.SMALL : iconSizes.CUSTOM}
+              ariaLabel={name === 'phone' ? phone.ariaLabel : mail.ariaLabel}
+              className='duration-250 cursor-pointer text-xs text-blue-90 transition-all ease-in-out hover:text-blue-90 focus:text-accent dark:text-blue-60 hover:dark:text-blue-60'
+            />
+            {label}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Contacts;
