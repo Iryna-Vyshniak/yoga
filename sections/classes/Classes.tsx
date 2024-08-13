b2dc7d0eefb29012fc0637e712264de3ef63941d
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-13 21:46:22 +0300

    feat: Implemented Classes section component
    - Created the `Classes` component to display the classes section on the yoga studio website.
    - Styled the component with full-screen height and flexible center alignment to ensure it's prominently displayed.
    - Applied conditional background gradients for light and dark themes using Tailwind CSS classes, enhancing visual appeal across different user preferences.
    - Ensured responsiveness and accessibility by using semantic HTML and responsive design practices.
    
    This component provides a dedicated space for showcasing yoga classes, making it easy for users to find and engage with the offerings of the studio.

---
+++ sections/classes/Classes.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Classes = () => {
+  return (
+    <section
+      id='classes'
+      className='flex h-screen items-center justify-center bg-light-gradient dark:bg-dark-gradient'
+    >
+      <div className='container relative flex h-full min-h-screen w-full flex-col items-center justify-center gap-4'>
+        CLASSES
+      </div>
+    </section>
+  );
+};
+
+export default Classes;
