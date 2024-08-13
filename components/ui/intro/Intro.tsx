64cd81569dcb0e6dc5775dbb5682d84ddc2ba761
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-13 21:19:40 +0300

    feat: Implement dynamic Intro component with auto-hide and animation
    - Developed the `Intro` component to display a branded introduction with animations for the Yoga Studio application.
    - Utilized the `Icon` component to display the logo dynamically with accessibility features, leveraging data from `common.json`.
    - Implemented an auto-hide feature with a 5-second timeout to enhance user experience by automatically transitioning from the introduction to the main content.
    - Styled the component using Tailwind CSS for responsive design, including animated gradients for text and background transitions between light and dark themes.
    - Ensured the intro fades out gracefully, improving the initial user interaction with the application.
    
    This update enhances the initial user experience by providing a visually appealing introduction that smoothly transitions out, setting a professional tone for the application.

---
+++ components/ui/intro/Intro.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+import Icon, {
+  commonIcons,
+  iconSizes,
+  iconTypes,
+} from '@/components/ui/icon/Icon';
+import data from '@/shared/data/common.json';
+
+const Intro = () => {
+  const [show, setShow] = useState<boolean>(true);
+  const { introTitle, introSubtitle } = data.intro;
+  const { ariaLabel } = data.icons.logo;
+
+  useEffect(() => {
+    let timerId;
+
+    timerId = setTimeout(() => {
+      setShow(false);
+    }, 5000);
+
+    return () => clearTimeout(timerId);
+  }, []);
+
+  if (!show) return null;
+
+  return (
+    <section className='remove-scrollbar fixed inset-0 z-50 flex h-screen w-screen items-center justify-center overflow-hidden bg-light-hero bg-cover bg-center bg-no-repeat dark:bg-dark-hero'>
+      <div className='flex flex-col items-center justify-center gap-4 rounded-sm p-4 backdrop-blur-sm'>
+        <Icon
+          type={iconTypes.COMMON}
+          name={commonIcons.LOGO}
+          size={iconSizes.EXTRA_LARGE}
+          ariaLabel={ariaLabel}
+          className='text-blue-90 dark:text-blue-60'
+        />
+        <h1 className='section-title text-transparent animate-typing-and-shine overflow-hidden whitespace-nowrap bg-dark-text-gradient bg-[length:200%] bg-clip-text font-tanNimbus uppercase drop-shadow-sm dark:bg-light-text-gradient md:p-4 lg:leading-[8rem]'>
+          {introTitle}
+        </h1>
+        <p className='animate-typing-and-shine overflow-hidden whitespace-nowrap bg-[length:200%] text-center text-base uppercase drop-shadow-sm sm:text-xl md:text-2xl'>
+          {introSubtitle}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
