025e271898ff558ce4befc90536f6d188cb09fbd
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-13 21:54:00 +0300

    feat: Implemented key UI components for Yoga Studio web application
    - Replaced the static layout with dynamic theme support using the `ThemeProvider` component from `next-themes`.
    - Integrated `Header`, `Intro`, `Hero`, and `Classes` components to enhance site navigation and user interaction.
    - Utilized custom hooks such as `useMediaMatch` to provide responsive design capabilities, ensuring the site adapts effectively across different device breakpoints.
    - Added comprehensive meta information including `title`, `description`, `keywords`, and `authors` to improve SEO.
    - Enriched global styles with `globals.css` for consistent theming and accessibility across all components.
    - Refined the home page layout to include major sections, improving the overall user flow and visual hierarchy.
    - Ensured accessibility features are in place, particularly with navigation and header components, to enhance usability for all users.
    
    These updates collectively enhance the site's aesthetics, functionality, and accessibility, making it more engaging and user-friendly.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,38 @@
-import type { Metadata } from 'next'
-import { Plus_Jakarta_Sans } from 'next/font/google'
+import type { Metadata } from 'next';
+import { Plus_Jakarta_Sans } from 'next/font/google';
 
-import { ThemeProvider } from '@/components/common/ThemeProvider'
-import { cn } from '@/lib/utils'
-import meta from '@/shared/data/meta.json'
+import { ThemeProvider } from '@/components/common/ThemeProvider';
+import Header from '@/layout/header/Header';
+import { cn } from '@/lib/utils';
+import meta from '@/shared/data/meta.json';
 
-import './globals.css'
+import './globals.css';
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-sans',
-})
+});
 
-const { title, description, keywords } = meta
+const { title, description, keywords, authors } = meta;
 
 export const metadata: Metadata = {
   title,
   description,
   keywords,
-}
+  authors,
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang='en' suppressHydrationWarning={true}>
       <body
         className={cn(
-          'remove-scrollbar text-slate-950 dark:text-white dark:via-slate-950 min-h-screen bg-light bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] bg-cover bg-no-repeat font-sans antialiased dark:bg-dark-300 dark:to-dark-200',
+          'remove-scrollbar text-slate-950 dark:via-slate-950 relative flex flex-col bg-light bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] bg-cover bg-no-repeat font-sans antialiased dark:bg-dark-300 dark:to-dark-200 dark:text-white',
           fontSans.variable,
         )}
       >
@@ -40,9 +42,10 @@ export default function RootLayout({
           enableSystem={true}
           enableColorScheme={true}
         >
-          {children}
+          <Header />
+          <main className='grow'>{children}</main>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- components/common/ThemeProvider.tsx
+++ components/common/ThemeProvider.tsx
@@ -1,10 +1,10 @@
-'use client'
+'use client';
 
-import * as React from 'react'
+import * as React from 'react';
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
