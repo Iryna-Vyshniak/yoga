254bed9b1eb84468e4150c60fde28c53fd3f485b
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-15 22:04:54 +0300

    feat: Created gallery data
    - Added three new images (YOGA1, YOGA2, YOGA3) to the public/assets/images directory for the yoga gallery feature.
    - Defined a new array `gallery` containing GalleryItem objects for each image, including identifiers, URLs, images, and descriptive content.
    - Each gallery item specifies a unique theme related to yoga practice such as balance, self-control, and harmony.
    
    This update introduces a structured gallery data set to enhance the visual representation of yoga practices and themes on the platform, aimed at engaging users with aesthetically pleasing and informative content.

---
+++ shared/data/gallery.tsx
@@ -0,0 +1,41 @@
+import YOGA1 from '@/public/assets/images/yoga-gallery-1.webp';
+import YOGA2 from '@/public/assets/images/yoga-gallery-2.webp';
+import YOGA3 from '@/public/assets/images/yoga-gallery-3.webp';
+
+import { GalleryItem } from '../types/type';
+
+export const gallery: GalleryItem[] = [
+  {
+    id: 'scroller-slide-1',
+    href: '#scroller-slide-1',
+    source: YOGA1,
+    label: '--scroller-slide-1',
+    num: '01',
+    title: 'YOGA vibe',
+    slogan: 'find balance',
+    description:
+      'Discover the harmony between body and mind by immersing yourself in a practice that helps you find balance in the modern world. Feel connected with nature and yourself.',
+  },
+  {
+    id: 'scroller-slide-2',
+    href: '#scroller-slide-2',
+    source: YOGA2,
+    label: '--scroller-slide-2',
+    num: '02',
+    title: 'YOGA vibe',
+    slogan: 'greater self control',
+    description:
+      'Strengthen your inner world and learn to better control your emotions and thoughts. Yoga practice helps you achieve greater self-discipline and harmony in life.',
+  },
+  {
+    id: 'scroller-slide-3',
+    href: '#scroller-slide-3',
+    source: YOGA3,
+    label: '--scroller-slide-3',
+    num: '03',
+    title: 'YOGA vibe',
+    slogan: 'embrace harmony',
+    description:
+      'Embrace harmony by combining physical exercises with spiritual practice. Yoga is the path to inner peace and unity with the world around you.',
+  },
+];
