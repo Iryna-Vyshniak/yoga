920751930b919531e5b6d57ebfcf9e6684584bc6
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-15 22:43:46 +0300

    feat: Implemented GallerySlider component for horizontal scrolling
    - Introduced GallerySlider component to facilitate horizontal scrolling of gallery items.
    - Enabled snap functionality to ensure each gallery item aligns correctly when scrolling.
    - Added custom CSS properties for smooth scrolling and snap behavior.
    - Integrated support for Scroll Timeline API to synchronize scrolling with gallery animations.
    - Utilized hidden CSS class to ensure support for browsers without Scroll Timeline API.

---
+++ components/common/gallery/GallerySlider.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { GallerySlideProps } from '@/shared/types/type';
+
+const GallerySlider: React.FC<GallerySlideProps> = ({ gallery }) => {
+  return (
+    <div
+      className='remove-scrollbar pointer-events-auto absolute inset-0 -z-10 hidden h-full w-full snap-x snap-mandatory overflow-x-auto scroll-smooth supports-sda:block'
+      style={{ scrollTimeline: '--scroller x' }}
+    >
+      <ul className='grid h-full w-fit auto-cols-[70cqw] grid-flow-col pr-[30cqw]'>
+        {gallery.map(({ id, label }) => (
+          <li
+            role='none'
+            key={id}
+            id={id}
+            className='snap-start'
+            style={{ viewTimeline: `${label} x` }}
+          ></li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default GallerySlider;
